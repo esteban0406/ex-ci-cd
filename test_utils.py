f75c800c654c729e96efb985bc3d97c47e0e04b5
Esteban <luisesteban0406@gmail.com>
2024-03-05 11:19:18 -0500

    Creacion ficheros

---
+++ test_utils.py
@@ -0,0 +1,9 @@
+from utils import suma
+
+def test_suma():
+    assert suma(2, 4) == 6
+    assert suma(5, 7) == 12
+    assert suma(10, 5) == 15 
+    assert suma(20, 15) == 35 
+    assert suma(20, 40) == 60 
+    assert suma(20, 20) == 40 
